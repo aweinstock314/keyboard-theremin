d0275884153cc6fa4d190ad5ab00f0d10fc88756
Avi Weinstock <aweinstock314@gmail.com>
2017-12-16 11:53:49 -0500

    Have UI and player on seperate threads.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate byteorder;
 extern crate glium;
 
 use std::process::{Command, Stdio};
+use std::sync::mpsc::{channel, Sender, Receiver};
 use std::io::Write;
 use byteorder::{ByteOrder, LittleEndian};
 use glium::{glutin, Surface};
@@ -18,8 +19,10 @@ fn triangle(x: f64) -> f64 {
     (x.fract()-0.5).abs()
 }
 
-fn play_note<F, G>(n: usize, amplitude: f64, freq: f64, wave: F, lfo: Box<G>) where
-    F: Fn(f64) -> f64 + Send + 'static, G: Fn(f64, f64) -> f64 + Send + 'static + ?Sized {
+struct Note<F: ?Sized, G: ?Sized>(usize, f64, f64, Box<F>, Box<G>);
+
+fn play_note<F, G>(Note(n, amplitude, freq, wave, lfo): Note<F,G>) where
+    F: Fn(f64) -> f64 + Send + 'static + ?Sized, G: Fn(f64, f64) -> f64 + Send + 'static + ?Sized {
     //let mut p = Command::new("strace").args(&["aplay", "-c", "2", "-f", "S32_LE", "-r", "16000"]).stdin(Stdio::piped()).spawn().unwrap();
     //let mut p = Command::new("xxd").stdin(Stdio::piped()).spawn().unwrap();
     //let mut p = Command::new("cat").stdin(Stdio::piped()).spawn().unwrap();
@@ -44,13 +47,8 @@ fn play_note<F, G>(n: usize, amplitude: f64, freq: f64, wave: F, lfo: Box<G>) wh
         }
     });
 }
-        
 
-fn main() {
-    //play_note(0);
-    /*for n in 0..8 {
-        play_note(n);
-    }*/
+fn ui(s: Sender<Note<Fn(f64) -> f64 + Send, Fn(f64, f64) -> f64 + Send>>) {
     let mut events_loop = glium::glutin::EventsLoop::new();
     let (width, height) = (1024, 768);
     let window = glium::glutin::WindowBuilder::new()
@@ -89,32 +87,32 @@ fn main() {
                         glutin::WindowEvent::ReceivedCharacter('1') => lfo = LFOType::Constant,
                         glutin::WindowEvent::ReceivedCharacter('2') => lfo = LFOType::Wave(f64::sin),
 
-                        glutin::WindowEvent::ReceivedCharacter('a') => play_note(0, ampmod*70.0, freqmod*700.0, f64::sin, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('s') => play_note(1, ampmod*70.0, freqmod*700.0, f64::sin, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('d') => play_note(2, ampmod*70.0, freqmod*700.0, f64::sin, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('f') => play_note(3, ampmod*70.0, freqmod*700.0, f64::sin, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('g') => play_note(4, ampmod*70.0, freqmod*700.0, f64::sin, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('h') => play_note(5, ampmod*70.0, freqmod*700.0, f64::sin, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('j') => play_note(6, ampmod*70.0, freqmod*700.0, f64::sin, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('k') => play_note(7, ampmod*70.0, freqmod*700.0, f64::sin, lfo_f),
+                        glutin::WindowEvent::ReceivedCharacter('a') => { let _ = s.send(Note(0, ampmod*70.0, freqmod*700.0, Box::new(f64::sin), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('s') => { let _ = s.send(Note(1, ampmod*70.0, freqmod*700.0, Box::new(f64::sin), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('d') => { let _ = s.send(Note(2, ampmod*70.0, freqmod*700.0, Box::new(f64::sin), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('f') => { let _ = s.send(Note(3, ampmod*70.0, freqmod*700.0, Box::new(f64::sin), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('g') => { let _ = s.send(Note(4, ampmod*70.0, freqmod*700.0, Box::new(f64::sin), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('h') => { let _ = s.send(Note(5, ampmod*70.0, freqmod*700.0, Box::new(f64::sin), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('j') => { let _ = s.send(Note(6, ampmod*70.0, freqmod*700.0, Box::new(f64::sin), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('k') => { let _ = s.send(Note(7, ampmod*70.0, freqmod*700.0, Box::new(f64::sin), lfo_f)); },
 
-                        glutin::WindowEvent::ReceivedCharacter('q') => play_note(0, ampmod*150.0, freqmod*700.0, square, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('w') => play_note(1, ampmod*150.0, freqmod*700.0, square, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('e') => play_note(2, ampmod*150.0, freqmod*700.0, square, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('r') => play_note(3, ampmod*150.0, freqmod*700.0, square, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('t') => play_note(4, ampmod*150.0, freqmod*700.0, square, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('y') => play_note(5, ampmod*150.0, freqmod*700.0, square, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('u') => play_note(6, ampmod*150.0, freqmod*700.0, square, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('i') => play_note(7, ampmod*150.0, freqmod*700.0, square, lfo_f),
+                        glutin::WindowEvent::ReceivedCharacter('q') => { let _ = s.send(Note(0, ampmod*150.0, freqmod*700.0, Box::new(square), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('w') => { let _ = s.send(Note(1, ampmod*150.0, freqmod*700.0, Box::new(square), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('e') => { let _ = s.send(Note(2, ampmod*150.0, freqmod*700.0, Box::new(square), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('r') => { let _ = s.send(Note(3, ampmod*150.0, freqmod*700.0, Box::new(square), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('t') => { let _ = s.send(Note(4, ampmod*150.0, freqmod*700.0, Box::new(square), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('y') => { let _ = s.send(Note(5, ampmod*150.0, freqmod*700.0, Box::new(square), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('u') => { let _ = s.send(Note(6, ampmod*150.0, freqmod*700.0, Box::new(square), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('i') => { let _ = s.send(Note(7, ampmod*150.0, freqmod*700.0, Box::new(square), lfo_f)); },
 
-                        glutin::WindowEvent::ReceivedCharacter('z') => play_note(0, ampmod*200.0, freqmod*800.0, triangle, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('x') => play_note(1, ampmod*200.0, freqmod*800.0, triangle, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('c') => play_note(2, ampmod*200.0, freqmod*800.0, triangle, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('v') => play_note(3, ampmod*200.0, freqmod*800.0, triangle, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('b') => play_note(4, ampmod*200.0, freqmod*800.0, triangle, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('n') => play_note(5, ampmod*200.0, freqmod*800.0, triangle, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter('m') => play_note(6, ampmod*200.0, freqmod*800.0, triangle, lfo_f),
-                        glutin::WindowEvent::ReceivedCharacter(',') => play_note(7, ampmod*200.0, freqmod*800.0, triangle, lfo_f),
+                        glutin::WindowEvent::ReceivedCharacter('z') => { let _ = s.send(Note(0, ampmod*200.0, freqmod*800.0, Box::new(triangle), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('x') => { let _ = s.send(Note(1, ampmod*200.0, freqmod*800.0, Box::new(triangle), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('c') => { let _ = s.send(Note(2, ampmod*200.0, freqmod*800.0, Box::new(triangle), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('v') => { let _ = s.send(Note(3, ampmod*200.0, freqmod*800.0, Box::new(triangle), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('b') => { let _ = s.send(Note(4, ampmod*200.0, freqmod*800.0, Box::new(triangle), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('n') => { let _ = s.send(Note(5, ampmod*200.0, freqmod*800.0, Box::new(triangle), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter('m') => { let _ = s.send(Note(6, ampmod*200.0, freqmod*800.0, Box::new(triangle), lfo_f)); },
+                        glutin::WindowEvent::ReceivedCharacter(',') => { let _ = s.send(Note(7, ampmod*200.0, freqmod*800.0, Box::new(triangle), lfo_f)); },
                         _ => (),
                     }
                 },
@@ -122,4 +120,17 @@ fn main() {
             }
         });
     }
+}
+
+fn player<F, G>(r: Receiver<Note<F, G>>) where
+    F: Fn(f64) -> f64 + Send + 'static + ?Sized, G: Fn(f64, f64) -> f64 + Send + 'static + ?Sized {
+    for note in r.iter() {
+        play_note(note);
+    }
+}
+
+fn main() {
+    let (s,r) = channel();
+    std::thread::spawn(move || ui(s));
+    player(r);
 }
